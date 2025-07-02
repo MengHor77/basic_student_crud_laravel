c65a47d5927f4ed229b8bb8ea49b7281739a9f9f
meng hor <piseth125125@gmail.com>
2025-07-02 20:22:57 +0700

    fix: naming route

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
 // List all active students
-Route::get('/student', [StudentController::class, 'index']);
+Route::get('/student', [StudentController::class, 'index'])->name('student.index');
 
 // Create student form
-Route::get('/create-student', [StudentController::class, 'createStudent']);
+Route::get('/create-student', [StudentController::class, 'create'])->name('student.create');
 
 // Create student POST
-Route::post('/create-student', [StudentController::class, 'storeStudent']);
+Route::post('/create-student', [StudentController::class, 'store'])->name('student.store');
 
 // Edit student form
-Route::get('/edit-student/{id}', [StudentController::class, 'editStudent']);
+Route::get('/edit-student/{id}', [StudentController::class, 'edit'])->name('student.edit');
 
 // Update student (PUT)
-Route::put('/update-student/{id}', [StudentController::class, 'submitUpdate']);
+Route::put('/update-student/{id}', [StudentController::class, 'update'])->name('student.update');
 
 // Soft delete student (DELETE)
-Route::delete('/delete-student/{id}', [StudentController::class, 'deleteStudent']);
+Route::delete('/delete-student/{id}', [StudentController::class, 'destroy'])->name('student.delete');
