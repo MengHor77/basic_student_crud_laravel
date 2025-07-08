20af7d73ee8a369bb0a1ca78c9ffe2c1730f70b9
meng hor <piseth125125@gmail.com>
2025-07-08 13:16:37 +0700

    fix: edit naming

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -49,7 +49,7 @@
                         <-Back to student list</a>
                 </div>
                 <button type="submit"
-                    class=" bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700">submit</button>
+                    class=" bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700">Submit</button>
             </div>
         </form>
     </div>
--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -49,7 +49,7 @@
                 </div>
                 <div class="bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700">
                     <button type="submit">
-                        Update Student
+                       Save Change
                     </button>
                 </div>
             </div>
